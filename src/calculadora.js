0c985322f69dfcca6edaaaafaec14ab4fe188ae4
livia <liviamaiahi@gmail.com>
2024-09-14 14:34:14 +0000

    sexto commit

--- src/calculadora.js
+++ src/calculadora.js
@@ -31,15 +31,3 @@
       multiply
     };
   
-   // Função de potenciação
-    function power(base, exponent) {
-        return Math.pow(base, exponent);
-    }
-
-    // Exportar as funções
-    module.exports = {
-      add,
-      subtract,
-      multiply,
-      power
-    };
