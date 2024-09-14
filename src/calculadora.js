faa8139275a72e989bdd6932b32d340acb03c6ac
livia <liviamaiahi@gmail.com>
2024-09-14 14:30:39 +0000

    quinto commit

--- src/calculadora.js
+++ src/calculadora.js
@@ -30,4 +30,19 @@
       subtract,
       multiply
     };
+
+      // Função de divisão
+    function divide(a, b) {
+        if (b === 0) {
+        throw new Error("Divisão por zero não é permitida.");
+        }
+        return a / b;
+    }
+
+    // Exportar as funções
+    module.exports = {
+      add,
+      subtract,
+      divide,
+    };
   
